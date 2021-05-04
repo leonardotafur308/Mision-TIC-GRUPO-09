bb8b0fab2c61422e17e9ee3ce6363cac526cc461
IsraelArbona <i.arbonammisena.edu.co>
2021-05-04 10:01:39 -0500

    inicio ciclo clase 2

---
+++ ejercicio4.py
@@ -0,0 +1,14 @@
+# crear funcion sin parametros
+
+def imprimirMes():
+    print('Grupo 09')
+
+# llamando la funcion
+imprimirMes()
+
+def imprimirMes_re():
+    return('Gurpo 09')
+
+texto = imprimirMes_re()
+print(texto)
+print(imprimirMes_re())
