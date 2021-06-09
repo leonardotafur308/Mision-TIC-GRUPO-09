c87a096a984e481ce8acd64068715fd0a3642108
IsraelArbona <i.arbonammisena.edu.co>
2021-06-09 13:53:56 -0500

    organizar semana 4 dia 09 junio

--- Semana 4/ejercicio12.py
+++ Semana 4/ejercicio12.py
@@ -23,16 +23,16 @@ list(zip(info[1],list(map(lambda x:x[-1:(len(x)+1)* -1:-1],info[1] )) )) )) )
 else 'False'
 )
 
-
+'''
 result_1 = list(map(int,info[1]))
 print(result_1)
 
 result_2 = all(list(map(lambda x: x>0, list(map(int,info[1])) )) )
 print(result_2)
 
-
 result_3 = any(list(map(lambda x: x[0] == x[1] or x[0] == '5',
 list(zip(info[1],list(map(lambda x:x[-1:(len(x)+1)* -1:-1],info[1] )) )) )) )
 
 result_3_1 = list(zip(info[1],list(map(lambda x:x[-1:(len(x)+1)* -1:-1],info[1] )) ))
 print(result_3_1)
+'''
